156c7a09dbfbbd5982c6562ba006fed38c32e932
joshikamlesh55 <72187420+joshikamlesh55@users.noreply.github.com>
2020-10-17 12:48:13 +0530

    Create TimeConversion.cpp

---
+++ TimeConversion.cpp
@@ -0,0 +1,51 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string timeConversion(string s) {
+    for(int i=0;s[i]!='\0';i++)
+    {
+        if(s[i]=='P')
+        {
+            if((s[0]=='1')&&(s[1]=='2'))
+            {
+            break;
+            }
+            else{
+            s[0]=s[0]+1;
+            s[1]=s[1]+2;
+            break;
+            }
+        }
+        else if(s[i]=='A')
+            {
+                if(s[0]=='1'&&s[1]=='2')
+                {
+             s[0]='0';
+            s[1]='0';
+            break;
+            }
+    }
+    }
+    s.pop_back(); //removes the last element of the string i.e M
+    s.pop_back(); //Removes second last element i.e P or A
+    return s;
+
+}
+
+int main()
+{
+    
+
+    string s;
+    cout<<"\n Enter Time as follow (HH:MM:SSXX) "; //HERE XX IS THE AM OR PM
+    getline(cin, s);
+    string result = timeConversion(s);
+
+    cout <<"\n The Coverted Format is "<< result << "\n";
+
+    
+
+    return 0;
+}
